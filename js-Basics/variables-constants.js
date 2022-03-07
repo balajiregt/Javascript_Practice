71cb675858557d449d84ecd3fc319f32e3634b89
BalajiK <71222979+balajiregt@users.noreply.github.com>
2022-03-07 08:42:56 +0530

    Update variables-constants.js
    update age

--- js-Basics/variables-constants.js
+++ js-Basics/variables-constants.js
@@ -1,7 +1,7 @@
 //Variables & Constants
 let firstName='balaji';
 firstName='balaji kumarasamy'; //dynamic variable data update
-let age=32;
+let age=33;
 const name='Adithi';
 //name='Buttu';  //if we want a constant data, then use const. It wont dynamically update like variables*/
 console.log(firstName);
