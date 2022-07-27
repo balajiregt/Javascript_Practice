031b80e2046dd7b3fb0250aae20b64e899c36b8b
BalajiK <71222979+balajiregt@users.noreply.github.com>
2022-07-27 08:26:18 +0530

    Add files via upload

---
+++ js-Basics/switch.js
@@ -0,0 +1,14 @@
+for(num=1; num<=100; num++){
+    let i=num
+    switch (i%3) {
+    case 0:
+        console.log("fuzz, buzz")
+        break;
+
+    default:
+       console.log(i)
+        break;
+}
+
+}
+  
